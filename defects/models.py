db10d0081ef3766c45bfb62222094c6924b2239c
Sergey Rubtsov <rsg81@ya.ru>
2021-08-18 16:59:19 +0300

    модель в разработке

--- defects/models.py
+++ defects/models.py
@@ -6,17 +6,18 @@ User = get_user_model()
 
 class TypeOfMismatch(models.Model):
     """Вид несоответствия"""
-    pass
+    mismatch = models.CharField(max_length=150, verbose_name='Вид несоответствия')
 
 
 class Details(models.Model):
     """Детали"""
-    pass
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    article = models.CharField(max_length=150, verbose_name='Артикул')
 
 
 class Bodies(models.Model):
-    """Кузова"""
-    name = models.CharField(max_length=150, verbose_name='Номер')
+    """Номера кузовов"""
+    body_number = models.CharField(max_length=150, verbose_name='Номер')
 
 
 class Workshops(models.Model):
@@ -31,4 +32,13 @@ class Workshops(models.Model):
 
 
 class Defects(models.Model):
-    pass
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_of_discovery = models.DateTimeField(verbose_name='Дата обнаружения')
+    term_up_to = models.DateTimeField(verbose_name='Срок до')
+    elimination_defect = models.BooleanField(default=False, verbose_name='Устранение дефекта')
+    workshop = models.ForeignKey(
+        Workshops,
+    )
+
+
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото дефекта', blank=True)

513d232a5485c096e1bfdab424060ffa6e824a5e
Sergey Rubtsov <rsg81@ya.ru>
2021-08-20 16:50:33 +0300

    11

--- bollore/urls.py
+++ bollore/urls.py
@@ -18,10 +18,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from defects.views import pageNotFound
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('defects.urls')),
 ]
+
+handler404 = pageNotFound
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- defects/views.py
+++ defects/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 def add_defect(request):
     return HttpResponse('Hello word')
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
